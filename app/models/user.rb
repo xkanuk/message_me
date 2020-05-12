4118dd801962f27c903f60446afd1ec9011ffb9f
Sherman Simpson <xkanuk@gmail.com>
2020-05-12 15:17:58 +1000

    add Message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false },
                       length: { minimum: 3, maximum: 15 }
   has_secure_password
+  has_many :messages
 end
